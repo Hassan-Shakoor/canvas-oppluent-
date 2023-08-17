9fb008937833c4e338c3e1762d20cfd1defa760e
zohaibamir203 <zohaibamir203@gmail.com>
2023-08-17 21:05:45 +0500

    Implement SideBar Content #9 (Map JSON Data)

--- src/components/CategContentComponent/CategoryContent.jsx
+++ src/components/CategContentComponent/CategoryContent.jsx
@@ -1,30 +1,163 @@
-import React from 'react';
+import React, { createFactory, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import DashboardHeader from './DashboardHeader';
+import Template from './Template';
+
+const categoriesData = [
+  {
+    "id": 1,
+    "subHeading": "",
+    "template": [
+      {
+        "id": 1,
+        "cardTitle": "TCG Logos",
+        "favorite": false,
+        "imageUrl": "/images/logo.png",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-16"  // Add the created date here
+      },
+      {
+        "id": 2,
+        "cardTitle": "Email Signature",
+        "favorite": true,
+        "imageUrl": "/images/Email_Signature-0 (1).jpg",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-15"  // Add the created date here
+      },
+      {
+        "id": 3,
+        "cardTitle": "Email Signature",
+        "favorite": false,
+        "imageUrl": "/images/Email_Signature-0 (2).jpg",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-14"  // Add the created date here
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "subHeading": "500 x 125",
+    "template": [
+      {
+        "id": 1,
+        "cardTitle": "Email Signature",
+        "favorite": false,
+        "imageUrl": "/images/Email_Signature-0.jpg",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-16"  // Add the created date here
+      },
+      {
+        "id": 2,
+        "cardTitle": "Email Signature",
+        "favorite": true,
+        "imageUrl": "/images/Email_Signature-0 (1).jpg",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-15"  // Add the created date here
+      },
+      {
+        "id": 3,
+        "cardTitle": "Email Signature",
+        "favorite": false,
+        "imageUrl": "/images/Email_Signature-0 (2).jpg",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-14"  // Add the created date here
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "subHeading": "HTML Email Newsletters are now available! HTML Email Newsletter Templates are built with code to be pasted or exported to your favorite email sender or CRM instead of just an image.",
+    "template": [
+      {
+        "id": 4,
+        "cardTitle": "HTML Email Newsletter",
+        "favorite": true,
+        "imageUrl": "/images/HTML_Email_Newsletter.jpg",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-13"  // Add the created date here
+      },
+      {
+        "id": 5,
+        "cardTitle": "HTML Email Newsletter",
+        "favorite": false,
+        "imageUrl": "/images/HTML_Email_Newsletter (1).jpg",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-12"  // Add the created date here
+      },
+      {
+        "id": 6,
+        "cardTitle": "Just Listed HTML Email Newsletter",
+        "favorite": false,
+        "imageUrl": "/images/Just_Listed_HTML_Email_Newsletter.jpg",
+        "modified": "2023-08-17", // Add the modified date here
+        "created": "2023-08-11"  // Add the created date here
+      }
+    ]
+  }
+];
+
 
 function CategoryContent() {
+  // Id Passed to the component from URL Parameter
   const { id } = useParams();
+  // Keeping the State of JSON. So, whenever JSON changes it rerender.
+  const [category , setCategory] = useState(null)
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchCategoryData = async () => {
+      try {
+        const fetchedCategory = await categoriesData.find(category => category.id === parseInt(id));
 
-  // Use the `id` to fetch and display the relevant content
-  let result
+        if (fetchedCategory) {
+          setCategory(fetchedCategory);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  switch (true){
-    case id >= 1 && id <= 51:
-      result =( 
-      <>
-        <div className="container">
-          <h1 className="dashboard-title">Welcome to Your Dashboard, {id} </h1>  
-        </div>
-      </>)
-      break;
-    default:
-      result = <DashboardHeader/>
+    fetchCategoryData();
+  }, [id]);
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
   }
 
-
   return (
+    
     <div className="page__content">
-        {result}
+      {id? 
+        <div className="container">
+        {/* Top Header */}
+            <div className="dashboard-header">
+        {/* Sorting Panel */}
+                <div className="dashboard-header__top-panel">
+                    <div className="dashboard-header__left-panel"></div>
+                    <div className="dashboard-header__right-panel">
+                    </div>
+                </div>
+        {/* Header Paragraph */}
+                <div className="dashboard-header__description">{category.subHeading}</div>
+            </div>
+        {/* Templates */}
+        <div className='infinite-scroll-component__outerdiv'>
+          <div className='infinite-scroll-component' style={{ height: "auto", overflow: "auto" }}>
+            <div className='waterfall-component'>
+              <span>
+              <div className="template-grid-container" style={{gridTemplateColumns: 'repeat(4, auto)'}}>
+                {category.template.map((item, index) => (
+                  <Template key={index} cardTitle = {item.cardTitle} imageUrl = {item.imageUrl}/>
+                ))}
+              </div>
+              </span>
+            </div>
+          </div>
+        </div>
+        </div>
+        :
+        <DashboardHeader/>
+      }
     </div>
     );
 }
---
+++ src/components/CategContentComponent/Template.jsx
@@ -0,0 +1,114 @@
+import React, { useState }  from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function MoreDropDown(){
+    return(
+        <div
+            className="rc-dropdown rc-dropdown-placement-bottomLeft"
+            style={{
+            inset: "100% 3% auto auto",
+            boxSizing: "border-box",
+            minWidth: "25.3335px"
+            }}
+        >
+            <ul
+                className="rc-menu rc-menu-root rc-menu-vertical"
+                role="menu"
+                tabIndex="0"
+                data-menu-list="true"
+            >
+                <li
+                    className="rc-menu-item"
+                    role="menuitem"
+                    tabIndex="-1"
+                    data-menu-id="rc-menu-uuid-15312-1-0"
+                >
+                    <button type="button" className="btn btn_menu-item">
+                        <span className="btn__text">Share</span>
+                    </button>
+                </li>
+                <li
+                    className="rc-menu-item"
+                    role="menuitem"
+                    tabIndex="-1"
+                    data-menu-id="rc-menu-uuid-15312-1-1"
+                >
+                    <button type="button" className="btn btn_menu-item">
+                        <span className="btn__text">Share link</span>
+                    </button>
+                </li>
+                <li
+                    className="rc-menu-item"
+                    role="menuitem"
+                    tabIndex="-1"
+                    data-menu-id="rc-menu-uuid-15312-1-2"
+                >
+                    <button type="button" className="btn btn_menu-item">
+                        <span className="btn__text">Edit Master Template</span>
+                    </button>
+                </li>
+            </ul>
+            <div aria-hidden="true" style={{ display: "none" }}></div>
+        </div>
+    )
+}
+
+function Template(props){
+    const [isMoreDropDown, setMoreDropDown] = useState(false);
+  
+    function dropdownHandler(){
+      setMoreDropDown(!isMoreDropDown)
+    }
+  
+    return(
+      <div className="template" style={{width: '280px'}}>
+        <div className="template__preview-wrapper">
+          <div className="template__preview">
+            <img
+              alt="Email Signature"
+              className="template__preview-image"
+              src= {props.imageUrl}
+            />
+            <button type="button" className="btn btn_no-min-width template__edit-btn">
+              <span className="btn__text">Create Design</span>
+            </button>
+          </div>
+          <div className="template__menu">
+            <div className="template__menu-btn-set">
+              <button
+                type="button"
+                className="btn btn_black btn_no-text template__menu-btn"
+              >
+                <span className="btn__text">
+                  <FontAwesomeIcon icon="fa-regular fa-heart" style={{color: "#7a7a7a",}} />
+                </span>
+              </button>
+            </div>
+            <button
+              type="button"
+              className="btn btn_black btn_no-text template__menu-dropdown"
+              data-test="design-menu-button"
+              onClick={dropdownHandler}
+            >
+              <svg className="icon v2-icon v2-icon-ellipsis-h">
+                <use href="#v2-icon-ellipsis-h" xlinkHref="#v2-icon-ellipsis-h" />
+              </svg>
+              <span className="btn__text" />
+              {/* Dropdown */}
+              {isMoreDropDown && <MoreDropDown/>}
+            </button>
+          </div>
+        </div>
+        <div className="template__panel">
+          <div
+            className="template__title template__title_editable"
+            title="Email Signature"
+          >
+            {props.cardTitle}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  export default Template;
