4c3345eead9ca7c3c84df50341a6437a626b0887
zohaibamir203 <zohaibamir203@gmail.com>
2023-11-12 20:06:03 +0500

    Stored Backend URL to frontend in ENV

--- client/src/shared/routes.js
+++ client/src/shared/routes.js
@@ -1,5 +1,6 @@
-const baseUrl = "https://claircius-backend.vercel.app"
+const baseUrl = process.env.REACT_APP_BACKEND_URL
 
+console.log(baseUrl)
 export const APIS = {
     searchPixabay: `${baseUrl}/searchPixabay`,
     generateQR: `${baseUrl}/generateQR`,
