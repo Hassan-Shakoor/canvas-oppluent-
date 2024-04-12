3b4b5f7d78b3bc36b95824ead83a04afd9e5255e
Muhammad Feroz <ferozamir00@gmail.com>
2024-04-12 17:29:29 +0500

    Popup option to Copy and Delele Canvases

--- client/src/components/EditComp/EdirPageManager.jsx/PageManagerList.jsx
+++ client/src/components/EditComp/EdirPageManager.jsx/PageManagerList.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
 import { useDispatch, useSelector } from "react-redux";
-import canvas, { selectCanvasContainer, selectResolution, selectSelectedCanvas, selectSelectedObject, updateSelectedCanvas } from "../../../store/app/Edit/Canvas/canvas";
+import canvas, { selectCanvasContainer, selectResolution, selectSelectedCanvas, selectSelectedObject, updateCanvasContainer, updateFabricData, updateSelectedCanvas } from "../../../store/app/Edit/Canvas/canvas";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTranslation } from "react-i18next";
+import ConfirmationModal from "../../Modal/ConfirmationModal";
+import { serializeCanvasContainer, setCanvasRef } from "../../../shared/utils/fabric";
 
 function PageManagerList(props) {
 
   const dispatch = useDispatch();
+  const { t } = useTranslation()
   const { width, height } = useSelector(selectResolution);
   const [previewWidth, setPreviewWidth] = useState(55);
   const [previewHeight, setPreviewHeight] = useState(30);
-
   const [firstTime, setFirstTime] = useState(true);
-
   const [snapshotURI, setSnapshotURI] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const [showDeletePageModal, setShowDeletePageModal] = useState(false);
 
   const canvasContainer = useSelector(selectCanvasContainer)
   const selectedCanvas = useSelector(selectSelectedCanvas)
@@ -87,17 +92,119 @@ function PageManagerList(props) {
     dispatch(updateSelectedCanvas(props.index));
   }
 
+  const handleMouseEnter = (e) => {
+    console.log(e);
+    setShowPopup(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    // Check if the mouse is actually leaving the list item
+    // If the mouse is moving to the popup, don't close it
+    if (!e.relatedTarget || !e.currentTarget.contains(e.relatedTarget)) {
+      setShowPopup(false);
+    }
+  };
+
+  const handleCopyPage = () => {
+
+    const indexToCopy = props.index;
+
+    const newCanvases = [...canvasContainer];
+    const canvasToCopy = newCanvases[indexToCopy];
+
+    if (indexToCopy < canvasContainer.length - 1) {
+      newCanvases.splice(indexToCopy + 1, 0, canvasToCopy);
+      const serialized = serializeCanvasContainer(newCanvases)
+      dispatch(updateFabricData(serialized))
+
+      console.log("Item removed from canvasContainer:", canvasContainer);
+    } else {
+      newCanvases.push(canvasToCopy)
+      const serialized = serializeCanvasContainer(newCanvases)
+      dispatch(updateFabricData(serialized))
+
+      console.log("Invalid index to remove from canvasContainer");
+    }
+  }
+
+  const handleDeletePage = (event) => {
+    event.preventDefault()
+
+    const indexToRemove = props.index;
+
+    const newCanvases = [...canvasContainer];
+
+    if (indexToRemove >= 0 && indexToRemove < canvasContainer.length) {
+      newCanvases.splice(indexToRemove, 1);
+
+      const serialized = serializeCanvasContainer(newCanvases)
+      setCanvasRef([...newCanvases])
+      dispatch(updateCanvasContainer([...newCanvases]));
+      dispatch(updateFabricData(serialized))
+
+
+      console.log("Item removed from canvasContainer:", canvasContainer);
+    } else {
+      console.log("Invalid index to remove from canvasContainer");
+    }
+    setShowDeletePageModal(false);
+  }
+
   return (
     <li
       draggable="true"
       id={props.id}
       className={`page-manager__stage-preview ${selectedCanvas === props.index ? 'page-manager__stage-preview_active' : ''}`}
       onClick={handleListClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       style={{
         width: previewWidth,
-        height: previewHeight
+        height: previewHeight,
       }}
     >
+      {showPopup && (
+        <div style={{
+          paddingRight: '8px',
+          width: '90px',
+          height: '100px',
+          position: 'absolute',
+          right: '52px',
+        }}>
+          <div className="popup-page-manager"
+            onMouseEnter={handleMouseEnter}
+            style={{
+              background: '#183062f0',
+              // marginRight: '8px',
+              borderRadius: '8px',
+              border: '1px solid',
+              padding: '10px'
+            }}>
+            <ul style={{ lineHeight: '1.6rem', color: "white", fontSize: '12px' }}>
+              <li style={{ display: 'flex' }} onClick={handleCopyPage} >
+                <div style={{ width: '20px' }}><FontAwesomeIcon icon="fa-regular fa-copy" /></div> Copy
+              </li>
+              <li style={{ display: 'flex' }}>
+                <div style={{ width: '20px', paddingLeft: '3px' }}>{props.index + 1}</div> Page
+              </li>
+              <li style={{ display: 'flex' }} onClick={() => setShowDeletePageModal(true)}>
+                <div style={{ width: '20px' }}><FontAwesomeIcon icon="fa-solid fa-trash" /></div> {t("delete")}
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+      {showDeletePageModal && (
+        <ConfirmationModal
+          title={<p style={{ color: '#000', margin: 0 }}>{t("EditHeader.deletePageConfirmation")}</p>}
+          body={<p style={{ color: '#000', margin: 0 }}>{t("EditHeader.deletePageConfirmationMessage")}</p>}
+          secondaryBtnTxt={t("cancel")}
+          primaryBtnTxt={t("delete")}
+          close={() => { setShowDeletePageModal(false) }}
+          submit={(event) => handleDeletePage(event)}
+        />
+      )}
+
       {snapshotURI && (
         <div style={{
           width: previewWidth,
